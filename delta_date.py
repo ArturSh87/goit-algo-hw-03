e9d382f86359d4261dde83ea8faf452da95dd14d
artur <ukdknd@gmail.com>
2025-05-26 15:18:27 +0300

    New_Commit

--- delta_date.py
+++ delta_date.py
@@ -5,7 +5,7 @@ def get_days_from_today(date_string):
         user_date = datetime.strptime(date_string, "%Y-%m-%d").date()
         current_date = date.today()
         delta = user_date - current_date  
-        return delta.days
+        return delta.days 
     except ValueError:
         return("Неправильний формат дати")
-print(get_days_from_today("2025-05-25"))
+print(get_days_from_today("2025-05-26"))
